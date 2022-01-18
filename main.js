06fccdc19cb06d16eded53faabe0f9af73e39ead
J-Kagotho <joycekagotho2002@gmail.com>
2022-01-18 03:56:04 -0500

    add delivery option plus the amount when pizza is delivered

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ function Pizza(toppings, size, delivery){
     this.size = size,
     this.delivery = delivery
   }
+  Pizza.prototype.deliveryAmount = function(){
+    if (this.delivery === "no"){
+      return 0;
+    } else if(this.delivery === "yes"){
+      return 200;
+    } else {
+      alert ("Please select delivery option");
+    }
+  }  
